8c4c6299beff3ec77551de84ac579503f24cdd09
Pau Garcia Martínez <etpau@hotmail.es>
2021-04-29 20:39:51 +0200

    Create basic counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,10 @@
 <!-- TEMPLATE -->
 <template>
   <h1>Vuex Counter</h1>
+  <h2 class="counter">0</h2>
+  <button>-</button>
+  <input type="number">
+  <button>+</button>
 </template>
 
 <!-- SCRIPT -->
@@ -12,5 +16,23 @@ export default {
 
 <!-- STYLE -->
 <style>
+.counter {
+  font-size: 5rem;
+}
+
+button {
+  border-radius: 100%;
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+input {
+  padding: 0.4rem;
+  margin: 0 0.5rem;
+  text-align: center;
+}
 
 </style>
